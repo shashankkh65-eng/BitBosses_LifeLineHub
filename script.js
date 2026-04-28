2b640c7f78cbbb6d8d9248c746e5469fdb2061e7
Sanjay S <sanjays294430@gmail.com>
2026-04-28 17:54:11 +0530

    feat: frontend donor module

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToDashboard() {
+    window.location.href = "dashboard.html";
+}
+
+function goToDonate() {
+    window.location.href = "donate.html";
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
